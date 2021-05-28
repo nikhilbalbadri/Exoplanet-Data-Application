dd0ef340946e50673330d5e49d5640491a58f610
Nikhil Balbadri <45367075+nikhilbalbadri@users.noreply.github.com>
2021-05-28 02:02:49 +0100

    Update OrphanPlanets.py

--- OrphanPlanets.py
+++ OrphanPlanets.py
@@ -1,4 +1,4 @@
-
+#Calculate number of Orphan planets
 def getOrphanPlanets(data):
     numOrphanPlanets = data[(data['HostStarMassSlrMass'] == '') & (data['HostStarRadiusSlrRad'] == '') & (data['HostStarAgeGyr'] == '')].shape[0]
     return numOrphanPlanets
