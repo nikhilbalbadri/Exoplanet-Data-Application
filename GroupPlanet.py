2fa737d7e1bc52dae2d78b8fd2d5c8adcebb5ebd
Nikhil Balbadri <45367075+nikhilbalbadri@users.noreply.github.com>
2021-05-28 02:02:03 +0100

    Update GroupPlanet.py

--- GroupPlanet.py
+++ GroupPlanet.py
@@ -1,8 +1,10 @@
+#Group planet by discovery year and Size
 def groupPlanet(data):
     data = data[data['RadiusJpt']!='']
-    data = data.assign(Size='')
+    data = data.assign(Size='') #Creating new column 'SIZE'
     data.loc[data['RadiusJpt'] > 2.0, 'Size'] = 'large'
     data.loc[data['RadiusJpt'] < 2.0, 'Size'] = 'medium'
     data.loc[data['RadiusJpt'] < 1.0, 'Size'] = 'small'
-    result = data.groupby(['DiscoveryYear','Size'])['PlanetIdentifier'].agg(['count']).apply(list).to_dict()['count']
+     #Grouping planet by discovery year and Size and converting it to html table format(in string)
+    result = data.groupby(['DiscoveryYear','Size']).size().to_frame('Number of Planets Discovered').to_html()
     return result
